889bd27ab0ae91ef71a5da3baad5db54942c9336
eskility <arntzn@gmail.com>
2023-12-04 21:17:44 +0100

    cleanup

--- 1/First.cs
+++ 1/First.cs
@@ -1,17 +1,12 @@
-public class First
+var lines = File.ReadAllLines("input.txt");
+var listOfNumbers = new List<int>();
+
+foreach (var line in lines)
 {
-    public static void Run()
-    {
-        var lines = File.ReadAllLines("input.txt");
-        var listOfNumbers = new List<int>();
-
-        foreach (var line in lines)
-        {
-            var first = line.Where(i => char.IsDigit(i)).First();
-            var second = line.Where(i => char.IsDigit(i)).Last();
-            listOfNumbers.Add(int.Parse(first.ToString() + second.ToString()));
-        }
-
-        Console.Write(listOfNumbers.Sum());
-    }
+    var first = line.Where(i => char.IsDigit(i)).First();
+    var second = line.Where(i => char.IsDigit(i)).Last();
+    listOfNumbers.Add(int.Parse(first.ToString() + second.ToString()));
 }
+
+Console.Write(listOfNumbers.Sum());
+
