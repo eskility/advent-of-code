08e53192438b8a6c71ed1f788c913fd25d27ef78
eskility <arntzn@gmail.com>
2023-12-04 18:31:02 +0100

    in prog

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,47 +1,7 @@
-﻿using System.Text;
-
-var lines = File.ReadAllLines("input.txt");
-var listOfNumbers = new List<int>();
-var textToNumbers = new Dictionary<string, string>
+﻿class Advent
 {
-   { "one","1"},
-   { "two","2"},
-   { "three","3"},
-   { "four","4"},
-   { "five","5"},
-   { "six","6"},
-   { "seven","7"},
-   { "eight","8"},
-   { "nine","9"}
-};
-
-foreach (var x in lines)
-{
-    if (x == "qeightwo2xjvfkfiveone")
-    { }
-    var sb = new StringBuilder();
-    var list = new List<string>();
-    foreach (var c in x)
+    public static void Main(string[] args)
     {
-        if (char.IsDigit(c))
-        {
-            list.Add(c.ToString());
-            sb = sb.Clear();
-        }
-        else
-        {
-            sb.Append(c);
-            if (!textToNumbers.Keys.Where(i => i.StartsWith(sb.ToString())).Any())
-                sb.Clear();
-
-            if (textToNumbers.ContainsKey(sb.ToString()))
-            {
-                list.Add(textToNumbers[sb.ToString()]);
-                sb = sb.Clear();
-            }
-        }
+        Second.Run();
     }
-    listOfNumbers.Add(int.Parse(list.First()+list.Last()));
 }
-
-Console.Write(listOfNumbers.Sum());
---
+++ 1/Second.cs
@@ -0,0 +1,53 @@
+using System.Text;
+
+public class Second
+{
+    public static void Run()
+    {
+        var lines = File.ReadAllLines("input.txt");
+        var listOfNumbers = new List<int>();
+        var textToNumbers = new Dictionary<string, string>
+{
+   { "one","1"},
+   { "two","2"},
+   { "three","3"},
+   { "four","4"},
+   { "five","5"},
+   { "six","6"},
+   { "seven","7"},
+   { "eight","8"},
+   { "nine","9"}
+};
+
+        foreach (var x in lines)
+        {
+            if (x == "qeightwo2xjvfkfiveone")
+            { }
+            var sb = new StringBuilder();
+            var list = new List<string>();
+            foreach (var c in x)
+            {
+                if (char.IsDigit(c))
+                {
+                    list.Add(c.ToString());
+                    sb = sb.Clear();
+                }
+                else
+                {
+                    sb.Append(c);
+                    if (!textToNumbers.Keys.Where(i => i.StartsWith(sb.ToString())).Any())
+                        sb.Clear();
+
+                    if (textToNumbers.ContainsKey(sb.ToString()))
+                    {
+                        list.Add(textToNumbers[sb.ToString()]);
+                        sb = sb.Clear();
+                    }
+                }
+            }
+            listOfNumbers.Add(int.Parse(list.First() + list.Last()));
+        }
+
+        Console.Write(listOfNumbers.Sum());
+    }
+}
