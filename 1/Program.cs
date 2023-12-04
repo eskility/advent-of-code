46b07fbe99b7baac87f86d3043aadd84bbd25507
eskility <arntzn@gmail.com>
2023-12-04 16:03:39 +0100

    typo

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,10 +1,8 @@
-﻿
-var lines = File.ReadAllLines("input.txt");
+﻿var lines = File.ReadAllLines("input.txt");
 var listOfNumbers = new List<int>();
 
 foreach (var line in lines)
 {
-
     var first = line.Where(i => char.IsDigit(i)).First();
     var second = line.Where(i => char.IsDigit(i)).Last();
     listOfNumbers.Add(int.Parse(first.ToString()+second.ToString()));
