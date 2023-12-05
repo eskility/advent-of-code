09348aefeb74123f08e7c96db229549c99828a39
eskility <arntzn@gmail.com>
2023-12-05 17:12:37 +0100

    Update 1.cs
    no need for cubic

--- 2/1.cs
+++ 2/1.cs
@@ -1,43 +1,24 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 var file = File.ReadLines("input.txt");
 int redLimit = 12, greenLimit = 13, blueLimit = 14;
 var gameIdSum = 0;
 foreach (var line in file)
 {
-    var gameId  = int.Parse(Regex.Match(line, @"\d+").Value);
+    var gameId = int.Parse(Regex.Match(line, @"\d+").Value);
     var picks = line.Split(";");
-
+    int blue = 0, red = 0, green = 0;
     foreach (var pick in picks)
     {
-        var colors = pick.Split(",");
-        int blue = 0, red = 0, green = 0;
-        foreach (var c in colors)
-        {
-            if (c.Contains("red"))
-            {
-                red = int.Parse(Regex.Match(c, @"\d+[ ]red").Value.Replace(" red", ""));
-            }
-
-            if (c.Contains("blue"))
-            {
-                blue = int.Parse(Regex.Match(c, @"\d+[ ]blue").Value.Replace(" blue", ""));
-            }
-
-            if (c.Contains("green"))
-            {
-                green = int.Parse(Regex.Match(c, @"\d+[ ]green").Value.Replace(" green", ""));
-            }
-        }
-
-        if (blue > blueLimit || red > redLimit || green > greenLimit)
-        {
-            gameId = 0;
-
-        }
-
+        if (pick.Contains("red"))
+            red = Math.Max(red, int.Parse(Regex.Match(pick, @"\d+[ ]red").Value.Replace(" red", "")));
+        if (pick.Contains("blue"))
+            blue = Math.Max(blue, int.Parse(Regex.Match(pick, @"\d+[ ]blue").Value.Replace(" blue", "")));
+        if (pick.Contains("green"))
+            green = Math.Max(green, int.Parse(Regex.Match(pick, @"\d+[ ]green").Value.Replace(" green", "")));
     }
+    if (red > redLimit || green > greenLimit || blue > blueLimit)
+        gameId = 0;
     gameIdSum += gameId;
 }
 Console.WriteLine(gameIdSum);
-
