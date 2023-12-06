56603d2b3a6d70b7420f7ecf3df5c626e7dab34f
eskility <arntzn@gmail.com>
2023-12-06 13:01:53 +0100

    intersect and math.Pow

--- 4/1.cs
+++ 4/1.cs
@@ -1,7 +1,7 @@
 using System.Text.RegularExpressions;
 
 var file = File.ReadLines("input.txt");
-double totalPoints = 0;
+int totalPoints = 0;
 
 foreach (var line in file)
 {
@@ -9,17 +9,8 @@ foreach (var line in file)
     var numbers = lineWithoutGameHeader.Split("|");
     var elfNumbers = Regex.Matches(numbers[0], @"\d+").Select(x => x.Value).ToList();
     var lotteryNumbers = Regex.Matches(numbers[1], @"\d+").Select(x => x.Value).ToList();
-    var matches = lotteryNumbers.Where(x => elfNumbers.Contains(x)).ToList();
-    var previous = 0;
-    if (matches.Count > 0)
-    {
-        previous = 1;
-        for (int i = 1; i < matches.Count; i++)
-            previous *= 2;
-    }
-    totalPoints += previous;
-
+    var matches = lotteryNumbers.Intersect(elfNumbers).Count();
+    totalPoints += (int)Math.Pow(2,matches-1);
 }
-
 Console.WriteLine(totalPoints);
 
