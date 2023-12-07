f43c4b78b659e30c96be817d83a297fb31c969f1
eskility <arntzn@gmail.com>
2023-12-07 17:04:00 +0100

    6-1

---
+++ 6/1.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+
+var file = File.ReadAllText("input.txt");
+var lines = file.Split("\n");
+
+var time = Regex.Matches(lines[0], @"\d+").Select(x => int.Parse(x.Value)).ToList();
+var distance = Regex.Matches(lines[1], @"\d+").Select(x => int.Parse(x.Value)).ToList();
+
+var races = new Race[time.Count];
+for (int i = 0; i < races.Length; i++)
+{
+    races[i] = new Race(time[i], distance[i]);
+}
+
+var raceBoat = new Boat();
+foreach (var race in races)
+{
+    for (int i = 0; i < race.Duration; i++)
+    {
+        raceBoat.HoldButton(i);
+        raceBoat.Race(race);
+    }
+}
+
+Console.Write(races.Select(x=> x.WaysToBeatTheRecord).Aggregate((x, y) => x * y));
+
+class Race(int _time, int _distance)
+{
+    public int RecordDistance { get; set; } = _distance;
+    public int Duration { get; set; } = _time;
+    public int WaysToBeatTheRecord { get; set; }
+}
+class Boat()
+{
+    public int Speed { get; set; }
+    public int ButtonHeld { get; set; }
+    public int DistanceCovered { get; set; }
+
+    public void HoldButton(int holdButton)
+    {
+        ButtonHeld = holdButton;
+        Speed = holdButton;
+    }
+    public void Race(Race race)
+    {
+        DistanceCovered = (race.Duration - ButtonHeld) * Speed;
+        if (DistanceCovered > race.RecordDistance)
+            race.WaysToBeatTheRecord++;
+    }
+
+}
