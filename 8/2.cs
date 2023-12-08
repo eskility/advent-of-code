6ce0c9475ebf52b4f4ebec1d1c203277c8b6e7d9
eskility <arntzn@gmail.com>
2023-12-08 23:05:21 +0100

    Update 2.cs

--- 8/2.cs
+++ 8/2.cs
@@ -28,9 +28,10 @@ void GraphSearcher(string node, int counter, List<char> directions, int directio
             directionCounter = 0;
 
         if (directions[directionCounter] == 'L')
-            GraphSearcher(graph[node][0], counter + 1, directions, directionCounter + 1, listOfPaths);
+            node = graph[node][0];
         else
-            GraphSearcher(graph[node][1], counter + 1, directions, directionCounter + 1, listOfPaths);
+            node = graph[node][1];
+        GraphSearcher(node, counter + 1, directions, directionCounter + 1, listOfPaths);
     }
 }
 
