5507e2b21dcfa2a59252f0a551a0466f60856b8d
eskility <arntzn@gmail.com>
2023-12-08 14:54:47 +0100

    cleanup

--- 7/2.cs
+++ 7/2.cs
@@ -5,17 +5,17 @@ var listOfHands = new List<Hand>();
 foreach (var line in lines)
 {
     var split = line.Split(" ");
-    var cardsData = split[0].ToList();
+    var cardArrayData = split[0].ToList();
     var bid = int.Parse(split[1]);
-    var cards = new int[15];
-    var cardsOrder = new List<int>();
-    foreach (var x in cardsData)
+    var cardArray = new List<int>();
+    foreach (var x in cardArrayData)
     {
         var card = Hand.CardLabelToRank(x);
-        cards[card]++;
-        cardsOrder.Add(card);
+        cardArray.Add(card);
+
     }
-    listOfHands.Add(new Hand(cards, bid, cardsOrder));
+
+    listOfHands.Add(new Hand(cardArray, bid));
 }
 var bidsSummed = 0;
 var listsorted = SelectionSort(listOfHands);
@@ -40,9 +40,9 @@ static List<Hand> SelectionSort(List<Hand> hands)
             if (hands[j].Strength == hands[smallestIndex].Strength)
             {
                 var counter = 0;
-                while (hands[j].CardsOrder[counter] == hands[smallestIndex].CardsOrder[counter])
+                while (hands[j].Cards[counter] == hands[smallestIndex].Cards[counter])
                     counter++;
-                if (hands[j].CardsOrder[counter] < hands[smallestIndex].CardsOrder[counter])
+                if (hands[j].Cards[counter] < hands[smallestIndex].Cards[counter])
                     smallestIndex = j;
             }
             else
@@ -50,7 +50,6 @@ static List<Hand> SelectionSort(List<Hand> hands)
                 if (hands[j].Strength < hands[smallestIndex].Strength)
                     smallestIndex = j;
             }
-
         }
         (hands[i], hands[smallestIndex]) = (hands[smallestIndex], hands[i]);
     }
@@ -60,57 +59,61 @@ class Hand
 {
     public int Rank { get; set; }
     public Set Strength { get; set; }
-    public int[] Cards { get; set; }
-    public List<int> CardsOrder { get; set; }
+    public List<int> Cards { get; set; }
     public int Bid { get; set; }
 
-    public Hand(int[] _cards, int _bid, List<int> _cardsOrder)
+    public Hand(List<int> _cards, int _bid)
     {
         Bid = _bid;
         Cards = _cards;
-        CardsOrder = _cardsOrder;
-        var jokers = Cards[1];
+        var cardArray = new int[15];
+
+        foreach (var card in Cards)
+            cardArray[card]++;
+
+      
+        var jokers = cardArray[1];
 
         var currentBest = Set.HighCard;
-        for (int i = 2; i < Cards.Length; i++)
+        for (int i = 2; i < cardArray.Length; i++)
         {
-            if (Cards[i] + jokers == 5)
+            if (cardArray[i] + jokers == 5)
             {
                 if (Set.FiveOfAKind > currentBest)
                     currentBest = Set.FiveOfAKind;
             }
 
-            else if (Cards[i] + jokers == 4)
+            else if (cardArray[i] + jokers == 4)
             {
                 if (Set.FourOfAKind > currentBest)
                     currentBest = Set.FourOfAKind;
             }
 
-            else if (Cards[i] + jokers == 3)
+            else if (cardArray[i] + jokers == 3)
             {
 
                 if (Set.ThreeOfAKind > currentBest)
 
                     currentBest = Set.ThreeOfAKind;
-                var jokersRemaining = jokers - (3 - Cards[i]);
-                for (int j = 2; j < Cards.Length; j++)
+                var jokersRemaining = jokers - (3 - cardArray[i]);
+                for (int j = 2; j < cardArray.Length; j++)
                 {
-                    if (j != 1 && i != j && Cards[j] + jokersRemaining == 2)
+                    if (j != 1 && i != j && cardArray[j] + jokersRemaining == 2)
                     {
                         if (Set.FullHouse > currentBest)
                             currentBest = Set.FullHouse;
                     }
                 }
             }
-            else if (Cards[i] + jokers == 2)
+            else if (cardArray[i] + jokers == 2)
             {
                 if (Set.OnePair > currentBest)
                     currentBest = Set.OnePair;
-                var jokersRemaining = jokers - (2 - Cards[i]);
+                var jokersRemaining = jokers - (2 - cardArray[i]);
 
-                for (int j = 2; j < Cards.Length; j++)
+                for (int j = 2; j < cardArray.Length; j++)
                 {
-                    if (j != 1 && i != j && Cards[j] + jokersRemaining == 2)
+                    if (j != 1 && i != j && cardArray[j] + jokersRemaining == 2)
                     {
                         if (Set.TwoPairs > currentBest)
                             currentBest = Set.TwoPairs;
@@ -120,7 +123,6 @@ class Hand
         }
         Strength = currentBest;
     }
-
     public enum Set
     {
         HighCard, OnePair, TwoPairs, ThreeOfAKind, FullHouse, FourOfAKind, FiveOfAKind
