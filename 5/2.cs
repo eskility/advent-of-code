28530d4d0ecb43458a5ed62e7d3395c8cda11a91
eskility <arntzn@gmail.com>
2023-12-09 19:13:04 +0100

    Update 2.cs

--- 5/2.cs
+++ 5/2.cs
@@ -2,62 +2,55 @@
 using System.Text.RegularExpressions;
 
 var file = File.ReadAllText("input.txt");
-
 var text = file.Split("\n\n");
 var inputs = Regex.Matches(text[0].Split(":")[1], @"\d+").Select(x => long.Parse(x.Value)).ToList();
-
-var seeds = new List<(long, long)>();
+var seeds = new Queue<(long, long)>();
 
 for (int i = 0; i < inputs.Count; i += 2)
 {
-    seeds.Add((inputs[i], inputs[i] + inputs[i + 1]));
+    seeds.Enqueue((inputs[i], inputs[i] + inputs[i + 1]));
 }
 
 foreach (var mapData in text.Skip(1))
 {
     var allMaps = mapData.Split("\n");
-    var map = new List<Map>();
+    var map = new Queue<Map>();
 
     foreach (var m in allMaps.Skip(1))
     {
         var numbers = Regex.Matches(m, @"\d+").Select(x => long.Parse(x.Value)).ToList();
-        map.Add(new Map(numbers[0], numbers[1], numbers[2]));
+        map.Enqueue(new Map(numbers[0], numbers[1], numbers[2]));
     }
-    var matches = new List<(long, long)>();
+    var matches = new Queue<(long, long)>();
     while (seeds.Count > 0)
     {
+        var x = seeds.Dequeue();
 
-        var x = seeds.Last();
-        seeds.RemoveAt(seeds.Count - 1);
         long s = x.Item1, e = x.Item2;
         var resultFound = false;
 
         foreach (var m in map)
         {
-
-            var overlapStart = Math.Max(s, m.b);
-            var overlapEnd = Math.Min(e, m.b + m.c);
+            var overlapStart = Math.Max(s, m.Source);
+            var overlapEnd = Math.Min(e, m.Source + m.Range);
             if (overlapStart < overlapEnd)
             {
                 resultFound = true;
-                matches.Add((overlapStart - m.b + m.a, overlapEnd - m.b + m.a));
+                matches.Enqueue((overlapStart - m.Source + m.Destination, overlapEnd - m.Source + m.Destination));
                 if (overlapStart > s)
                 {
-
-                    seeds.Add((s, overlapStart));
+                    seeds.Enqueue((s, overlapStart));
                 }
                 if (e > overlapEnd)
                 {
-
-                    seeds.Add((overlapEnd, e));
+                    seeds.Enqueue((overlapEnd, e));
                 }
 
                 break;
             }
-
         }
         if (!resultFound)
-            matches.Add((s, e));
+            matches.Enqueue((s, e));
     }
     seeds = matches;
 }
@@ -66,7 +59,7 @@ Console.WriteLine(seeds.Min());
 
 class Map(long _destination, long _source, long _range)
 {
-    public long a { get; set; } = _destination;
-    public long b { get; set; } = _source;
-    public long c { get; set; } = _range;
+    public long Destination { get; set; } = _destination;
+    public long Source { get; set; } = _source;
+    public long Range { get; set; } = _range;
 }
