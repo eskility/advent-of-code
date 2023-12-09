d21c51518a9a6993de728367d39fe9b2862ac255
eskility <arntzn@gmail.com>
2023-12-09 16:07:50 +0100

    part 2

---
+++ 9/2.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+var file = File.ReadAllText("input.txt");
+var lines = file.Split("\n");
+var result = 0;
+foreach (var line in lines)
+{
+    var numbers = new List<List<int>>
+    {
+        Regex.Matches(line, @"-?\d+").Select(x=> int.Parse(x.Value)).ToList()
+    };
+    numbers = FindDifferences(numbers);
+    result += Extrapolate(numbers);
+}
+Console.Write(result);
+
+static int Extrapolate(List<List<int>> differences)
+{
+    differences.Last().Insert(0, 0);
+    for (int i = differences.Count - 2; i >= 0; i--)
+    {
+        differences[i].Insert(0, differences[i].First() - differences[i + 1].First());
+    }
+    return differences[0].First();
+}
+
+static List<List<int>> FindDifferences(List<List<int>> differences)
+{
+    var lastDifferences = differences.Last();
+    if (lastDifferences.Where(i => i == 0).Count() == lastDifferences.Count)
+        return differences;
+    var newDifferences = new List<int>();
+    for (int i = 0; i < lastDifferences.Count - 1; i++)
+    {
+        newDifferences.Add(lastDifferences[i + 1] - lastDifferences[i]);
+    }
+    differences.Add(newDifferences);
+    return FindDifferences(differences);
+}
