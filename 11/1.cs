1caf5a0af14e4bd3496008a478a45a3708937a6d
eskility <arntzn@gmail.com>
2023-12-11 20:43:07 +0100

    using the idea from part 2 to make it part 1 cleaner

--- 11/1.cs
+++ 11/1.cs
@@ -1,9 +1,9 @@
-using System.Text;
+using System.Data;
 
 var file = File.ReadAllText("input.txt");
 var lines = file.Split("\n");
-var newLines = new List<string>();
-var columns = new List<int>();
+var expandedColumns = new List<int>();
+var expandedRows = new List<int>();
 
 for (int column = 0; column < lines[0].Length; column++)
 {
@@ -14,60 +14,54 @@ for (int column = 0; column < lines[0].Length; column++)
             emptyColumn = false;
     }
     if (emptyColumn)
-        columns.Add(column);
-}
-
-foreach (var line in lines)
-{
-    var sb = new StringBuilder();
-    sb.AppendLine(line);
-
-    for (int i = 0; i < columns.Count; i++)
-        sb.Insert(columns[i] + i, ".");
-    newLines.Add(sb.ToString());
-    if (line.Where(x => x == '.').Count() == line.Length)
-    {
-        newLines.Add(sb.ToString());
-    }
+        expandedColumns.Add(column);
 }
 
 var listOfNodes = new List<Node>();
 var starCounter = 1;
-for (int row = 0; row < newLines.Count; row++)
+for (int row = 0; row < lines.Length; row++)
 {
-    for (int column = 0; column < newLines[row].Length; column++)
+    var starFound = false;
+    for (int column = 0; column < lines[row].Length; column++)
     {
-
-        var node = new Node((row, column));
-
-      if (newLines[row][column] == '#')
+        if (lines[row][column] == '#')
         {
+            var node = new Node((row, column));
+            starFound = true;
             node.Id = starCounter++;
             listOfNodes.Add(node);
         }
-    
     }
+    if (!starFound)
+        expandedRows.Add(row);
 }
 
 var pairs = new List<((int, int), (int, int))>();
-var totalsteps = 0;
+long totalsteps = 0;
 foreach (var x in listOfNodes)
 {
     foreach (var y in listOfNodes)
         if (x != y && !pairs.Contains((x.Location, y.Location)) && !pairs.Contains((y.Location, x.Location)))
         {
-            pairs.Add((x.Location, y.Location));
-            totalsteps += Math.Abs(x.Location.Item1 - y.Location.Item1) + Math.Abs(x.Location.Item2 - y.Location.Item2);
-        }
+            var a = Math.Min(x.Location.Item1, y.Location.Item1);
+            var d = Math.Max(x.Location.Item2, y.Location.Item2);
+            var b = Math.Max(x.Location.Item1, y.Location.Item1);
+            var c = Math.Min(x.Location.Item2, y.Location.Item2);
 
+            int e = expandedRows.Where(x => x > a && x < b).Count();
+            int f = expandedColumns.Where(x => x > c && x < d).Count();
+
+            totalsteps += Math.Abs(b - a) + Math.Abs(d - c);
+            totalsteps += e + f;
+            pairs.Add((x.Location, y.Location));
+        }
 }
 
 Console.WriteLine(totalsteps);
-
 
 class Node((int, int) _location)
 {
     public int Id { get; set; }
     public (int, int) Location { get; set; } = _location;
-    
+
 }
