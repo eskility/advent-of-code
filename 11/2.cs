45cddf2600d05798d9f53a91e81505f4221deed3
eskility <arntzn@gmail.com>
2023-12-11 18:52:04 +0100

    Update 2.cs

--- 11/2.cs
+++ 11/2.cs
@@ -1,11 +1,10 @@
 using System.Data;
-using System.Text;
 
 var file = File.ReadAllText("input.txt");
 var lines = file.Split("\n");
 var newLines = new List<string>();
-var columns = new List<int>();
-var rows = new List<int>();
+var expandedColumns = new List<int>();
+var expandedRows = new List<int>();
 
 for (int column = 0; column < lines[0].Length; column++)
 {
@@ -17,17 +16,16 @@ for (int column = 0; column < lines[0].Length; column++)
 
     }
     if (emptyColumn)
-        columns.Add(column);
+        expandedColumns.Add(column);
 }
 
 for (int i = 0; i < lines.Length; i++)
 {
     if (lines[i].Where(x => x == '.').Count() == lines[i].Length)
     {
-        rows.Add(i);
+        expandedRows.Add(i);
     }
     newLines.Add(lines[i]);
-
 }
 
 var listOfNodes = new List<Node>();
@@ -55,20 +53,17 @@ foreach (var x in listOfNodes)
     foreach (var y in listOfNodes)
         if (x != y && !pairs.Contains((x.Location, y.Location)) && !pairs.Contains((y.Location, x.Location)))
         {
-            var xx = Math.Max(x.Location.Item2, y.Location.Item2);
-            var yy = Math.Min(x.Location.Item2, y.Location.Item2);
-            var zz = Math.Max(x.Location.Item1, y.Location.Item1);
-            var vv = Math.Min(x.Location.Item1, y.Location.Item1);
+            var a = Math.Max(x.Location.Item2, y.Location.Item2);
+            var b = Math.Min(x.Location.Item2, y.Location.Item2);
+            var c = Math.Max(x.Location.Item1, y.Location.Item1);
+            var d = Math.Min(x.Location.Item1, y.Location.Item1);
+            long e = expandedColumns.Where(x => x > b && x < a).Count();
+            long f = expandedRows.Where(x => x > d && x < c).Count();
 
-            long columncount = columns.Where(x => x > yy && x < xx).Count();
-            long rowcount = rows.Where(x => x > vv && x < zz).Count();
 
+            totalsteps += Math.Abs(a - b) + Math.Abs(c - d);
+            totalsteps += (e * 1000000) - e + (f * 1000000) - f;
             pairs.Add((x.Location, y.Location));
-
-
-            totalsteps += Math.Abs(xx - yy) + Math.Abs(zz - vv);
-           totalsteps += (columncount*1000000)-columncount + (rowcount*1000000)-rowcount;
-
         }
 }
 
