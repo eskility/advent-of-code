d7a657eabce2f94a5c22816a58ffe46a1e7439cc
eskility <arntzn@gmail.com>
2023-12-13 18:58:03 +0100

    cleaner approach

--- 13/1.cs
+++ 13/1.cs
@@ -1,95 +1,53 @@
-﻿using System.Data;
-using System.Globalization;
-using System.IO.Compression;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 var file = File.ReadAllText("input.txt");
 var input = file.Split("\n\n");
-
-var matchedRows2 = new List<int>();
-var matchedColumns = new List<int>();
-for (int i = 0; i < input.Length; i++)
-{
-    var columndata = input[i].Split("\n");
-
-    var j = columndata[0].Length - 1;
-    var listX = new List<string>();
-    var listY = new List<string>();
-    for (int column = 0; column < columndata[0].Length && j > i; column++)
-    {
-        var columnX = new StringBuilder();
-        var columnY = new StringBuilder();
-
-
-        for (int row = 0; row < columndata.Length; row++)
-        {
-            columnX.Append(columndata[row][column]);
-            columnY.Append(columndata[row][j]);
-
-        }
-        listX.Add(columnX.ToString());
-        listY.Add(columnY.ToString());
-        if (listX.Last() == listX.Last())
-        {
-            var mirror = true;
-
-            for (int z = 0; z <=Math.Min(column, columndata[0].Length - 1 - column); z++)
-            {
-                if (listX[z] != listY[z])
-                    mirror = false;
-            }
-
-
-
-            if (mirror)
-                matchedColumns.Add(column);
-        }
-
-        j--;
-    }
-
-
-
-
-    var rowdata = input[i].Split("\n");
-    for (int row = 0; row < rowdata.Length - 1; row++)
-    {
-        if (rowdata[row] == rowdata[row + 1])
-        {
-            var mirror = true;
-            int x = row, y = row + 1; ;
-            while (x >= 0 && y <= rowdata.Length - 1)
-            {
-                if (rowdata[x] != rowdata[y])
-                {
-                    mirror = false;
-                    break;
-                }
-                x--; y++;
-            }
-            if (mirror)
-                matchedRows2.Add((row));
-        }
-    }
-}
-
-
-
-
-
-
-
-
 var sum = 0;
 
-for (int i = 0; i < matchedRows2.Count; i++)
+for (int i = 0; i < input.Length; i++)
 {
-    sum += ((matchedRows2[i] + 1) * 100);
-}
-for (int i = 0; i < matchedColumns.Count; i++)
-{
-    sum += matchedColumns[i] + 1;
-}
+    var rows = input[i].Split("\n");
+    var columns = new List<string>();
 
+    for (int column = 0; column < rows[0].Length; column++)
+    {
+        var sb = new StringBuilder();
+
+        for (int row = 0; row < rows.Length; row++)
+        {
+            sb.Append(rows[row][column]);
+        }
+        columns.Add(sb.ToString());
+    }
+    sum += FindReflection(columns.ToArray(), false);
+    sum += FindReflection(rows, true);
+
+}
 Console.WriteLine(sum);
+
+static int FindReflection(string[] data, bool horizontal)
+{
+    for (int x = 0; x < data.Length - 1; x++)
+    {
+        if (data[x] == data[x + 1])
+        {
+            var reflection = true;
+            int y = x, z = x + 1;
+            while (y >= 0 && z <= data.Length - 1)
+            {
+                if (data[y] != data[z])
+                {
+                    reflection = false;
+                    break;
+                }
+                y--; z++;
+            }
+            if (reflection)
+                if (horizontal)
+                    return ((x + 1) * 100);
+                else
+                    return x + 1;
+        }
+    }
+    return 0;
+}
