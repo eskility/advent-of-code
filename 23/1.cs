01ef4c15805028914ab49e55ce1561ffc4ed7df3
eskility <arntzn@gmail.com>
2023-12-23 17:08:06 +0100

    Update 1.cs

--- 23/1.cs
+++ 23/1.cs
@@ -1,50 +1,52 @@
 var data = File.ReadAllText("input.txt").Split("\n");
-List<(int,int)> seen = [];
+List<(int, int)> seen = [];
 List<int> depths = [];
-Bfs(0,seen,(0,1),data,depths);
+Bfs(0, "", (0, 1), data, depths);
 
-void Bfs(int depth, List<(int,int)> seen,(int,int) location, string[] data,List<int> depths)
+void Bfs(int depth, string path, (int, int) location, string[] data, List<int> depths)
 {
-    var directions = new List<(int,int)>();
-    
-    if(  data[location.Item1][location.Item2] !='#')
+    var directions = new List<(int, int)>();
+
+    if (data[location.Item1][location.Item2] != '#')
     {
         depths.Add(depth);
-              seen.Add(location);
+        path += location;
+        if (data[location.Item1][location.Item2] == 'v')
+        {
+            directions.Add((location.Item1 + 1, location.Item2));
+        }
+        else if (data[location.Item1][location.Item2] == '^')
+        {
+            directions.Add((location.Item1 - 1, location.Item2));
+        }
+        else if (data[location.Item1][location.Item2] == '>')
+        {
+            directions.Add((location.Item1, location.Item2 + 1));
+        }
+        else if (data[location.Item1][location.Item2] == '<')
+        {
+            directions.Add((location.Item1, location.Item2 - 1));
+        }
 
+        else
+        {
+            directions.Add((location.Item1 - 1, location.Item2)); 
+            directions.Add((location.Item1 + 1, location.Item2)); 
+            directions.Add((location.Item1, location.Item2 + 1)); 
+            directions.Add((location.Item1, location.Item2 - 1));
+        }
 
-    if( data[location.Item1][location.Item2]=='v')
-    {
-directions.Add((location.Item1+1, location.Item2));
+        foreach (var direction in directions)
+        {
+            if (direction.Item1 >= 0 && direction.Item1 < data.Length && direction.Item2 >= 0 && direction.Item2 < data[0].Length && !path.Contains("" + direction))
+            {
+                if (directions.Count > 1)
+                    Bfs(depth + 1, path.ToString(), direction, data, depths);
+                else
+                    Bfs(depth + 1, path, direction, data, depths);
+            }
+        }
     }
-    else    if( data[location.Item1][location.Item2]=='^')
-    {
-directions.Add((location.Item1-1, location.Item2));
-    }
-     else  if( data[location.Item1][location.Item2]=='>')
-    {
-directions.Add((location.Item1, location.Item2+1));
-    }
-      else  if( data[location.Item1][location.Item2]=='<')
-    {
-directions.Add((location.Item1, location.Item2-1));
-    }
-
-else{
-    directions.Add((location.Item1-1, location.Item2));directions.Add((location.Item1+1, location.Item2));directions.Add((location.Item1, location.Item2+1));directions.Add((location.Item1,location.Item2-1));
-}
-
-foreach(var direction in directions)
-{
-    if(direction.Item1>=0 && direction.Item1<data.Length && direction.Item2 >=0 && direction.Item2<data[0].Length && !seen.Contains(direction))
-    {
-         if(directions.Count>1)
-         Bfs(depth+1,seen.ToList(),direction,data,depths);
-         else
-         Bfs(depth+1,seen,direction,data,depths);
-    } 
-}
-}
 }
 
 Console.WriteLine(depths.Max());
