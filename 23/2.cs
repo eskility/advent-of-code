8a05127d667ec8fa1d48b85d30de3ebdb7935db2
eskility <arntzn@gmail.com>
2023-12-28 20:21:20 +0100

    Update 2.cs

--- 23/2.cs
+++ 23/2.cs
@@ -1,6 +1,3 @@
-
-using System.Data;
-
 var data = File.ReadAllText("input.txt").Split("\n");
 var graph = new Dictionary<(int, int), List<(int, int, int)>>();
 
@@ -10,7 +7,6 @@ for (int row = 0; row < data.Length; row++)
     {
         if (data[row][column] != '#')
         {
-
             var connections = new List<(int, int)>();
             if (row > 0 && data[row - 1][column] != '#')
                 connections.Add((row - 1, column));
@@ -23,114 +19,53 @@ for (int row = 0; row < data.Length; row++)
 
 
             if (!graph.ContainsKey((row, column)))
-            {
                 graph[(row, column)] = [];
-            }
-            foreach (var connection in connections)
-                if (!graph[(row, column)].Contains((connection.Item1, connection.Item2, 0)))
-                    graph[(row, column)].Add((connection.Item1, connection.Item2, 0));
 
+            foreach (var connection in connections)
+                if (!graph[(row, column)].Contains((connection.Item1, connection.Item2, 1)))
+                    graph[(row, column)].Add((connection.Item1, connection.Item2, 1));
         }
 
     }
 }
 
-
-
-var corridors = graph.Where(n => n.Value.Count == 2)
-    .Select(node => node.Key)
-    .ToList();
-foreach (var (r, c) in corridors)
+//combine all the nodes in a corridor together by using edge contraction.
+// then we can run DFS on just the junctions and add the depth of the corridors between them
+foreach (var (r, c) in graph
+    .Where(n => n.Value.Count == 2)
+    .Select(node => node.Key))
 {
     var neighbors = graph[(r, c)];
     var (r1, c1, d1) = neighbors[0];
     var (r2, c2, d2) = neighbors[1];
     var n1 = graph[(r1, c1)];
-    var i = n1.FindIndex(item => (item.Item1, item.Item2) == (r, c));
-    if (i != -1)
-    {
-        n1[i] = (r2, c2, d1 + d2);
-    }
+
+    var i = n1.FindIndex(x => (x.Item1, x.Item2) == (r, c));
+    n1[i] = (r2, c2, d1 + d2);
+
     var n2 = graph[(r2, c2)];
-    i = n2.FindIndex(item => (item.Item1, item.Item2) == (r, c));
-    if (i != -1)
-    {
-        n2[i] = (r1, c1, d1 + d2);
-    }
+    i = n2.FindIndex(x => (x.Item1, x.Item2) == (r, c));
+    n2[i] = (r1, c1, d1 + d2);
 }
+
 List<int> paths = [];
 var seen = new List<(int, int)>();
-seen.Add((0, 1));
-df(0, seen, (0, 1), (data.Length - 1, data.Length - 2), graph, paths);
-DFS(0, seen, (0, 1), (data.Length - 1, data.Length - 2), graph, paths);
 
+DFS(0, seen, (0, 1), (data.Length - 1, data.Length - 2), graph, paths);
 
 void DFS(int depth, List<(int, int)> seen, (int, int) location, (int, int) target, Dictionary<(int, int), List<(int, int, int)>> graph, List<int> paths)
 {
     if (location == target)
     {
-
+        paths.Add(depth);
     }
-    else
+    else if (!seen.Contains(location))
     {
-
+        seen.Add(location);
         foreach (var direction in graph[location])
         {
-            if (!seen.Contains((direction.Item1, direction.Item2)))
-            {
-                seen.Add((direction.Item1, direction.Item2));
-                DFS(depth + 1, seen.ToList(), (direction.Item1, direction.Item2), target, graph, paths);
-
-            }
+            DFS(depth + direction.Item3, seen.ToList(), (direction.Item1, direction.Item2), target, graph, paths);
         }
     }
 }
 Console.WriteLine(paths.Max());
-
-void df(int depth, List<(int, int)> seen, (int, int) location, (int, int) target, Dictionary<(int, int), List<(int, int, int)>> graph, List<int> paths)
-{
-    if (location == target)
-        paths.Add(depth);
-    else
-    {
-
-        var directions = new List<(int, int)>();
-        if (data[location.Item1][location.Item2] == 'v')
-        {
-            directions.Add((location.Item1 + 1, location.Item2));
-        }
-        else if (data[location.Item1][location.Item2] == '^')
-        {
-            directions.Add((location.Item1 - 1, location.Item2));
-        }
-        else if (data[location.Item1][location.Item2] == '>')
-        {
-            directions.Add((location.Item1, location.Item2 + 1));
-        }
-        else if (data[location.Item1][location.Item2] == '<')
-        {
-            directions.Add((location.Item1, location.Item2 - 1));
-        }
-
-        else
-        {
-            directions.Add((location.Item1 - 1, location.Item2));
-            directions.Add((location.Item1 + 1, location.Item2));
-            directions.Add((location.Item1, location.Item2 + 1));
-            directions.Add((location.Item1, location.Item2 - 1));
-        }
-
-        foreach (var direction in directions)
-        {
-            if (direction.Item1 >= 0 && direction.Item1 < data.Length && direction.Item2 >= 0
-            && direction.Item2 < data[0].Length && data[direction.Item1][direction.Item2] != '#'
-            && !seen.Contains(direction))
-            {
-                seen.Add(direction);
-                        graph[location].Add((direction.Item1,direction.Item2,()
-                //make the seen list unique for every path by copying it using ToList()
-                df(depth + 1, seen.ToList(), direction, target, graph, paths);
-            }
-        }
-    }
-}
