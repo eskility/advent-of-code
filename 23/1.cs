d2194666fc856961a6f8ee72464bfe07021b5115
eskility <arntzn@gmail.com>
2023-12-28 14:37:48 +0100

    Update 1.cs

--- 23/1.cs
+++ 23/1.cs
@@ -1,15 +1,19 @@
+
 var data = File.ReadAllText("input.txt").Split("\n");
-List<int> finishedSteps = [];
-DFS(0, "", (0, 1), data, finishedSteps);
+List<int> paths = [];
 
-void DFS(int steps, string path, (int, int) location, string[] data, List<int> finishedSteps)
+DFS(0, new List<(int, int)>(), (0, 1), (data.Length - 1, data[0].Length - 2), data, paths);
+
+Console.WriteLine(paths.Max());
+
+void DFS(int depth, List<(int, int)> seen, (int, int) location, (int, int) target, string[] data, List<int> paths)
 {
-    var directions = new List<(int, int)>();
-
-    if (data[location.Item1][location.Item2] != '#')
+    if (location == target)
+        paths.Add(depth);
+    else
     {
-        finishedSteps.Add(steps);
-        path += location;
+
+        var directions = new List<(int, int)>();
         if (data[location.Item1][location.Item2] == 'v')
         {
             directions.Add((location.Item1 + 1, location.Item2));
@@ -29,19 +33,22 @@ void DFS(int steps, string path, (int, int) location, string[] data, List<int> f
 
         else
         {
-            directions.Add((location.Item1 - 1, location.Item2)); 
-            directions.Add((location.Item1 + 1, location.Item2)); 
-            directions.Add((location.Item1, location.Item2 + 1)); 
+            directions.Add((location.Item1 - 1, location.Item2));
+            directions.Add((location.Item1 + 1, location.Item2));
+            directions.Add((location.Item1, location.Item2 + 1));
             directions.Add((location.Item1, location.Item2 - 1));
         }
 
         foreach (var direction in directions)
         {
-            if (direction.Item1 >= 0 && direction.Item1 < data.Length && direction.Item2 >= 0 && direction.Item2 < data[0].Length && !path.Contains("" + direction))
-            
-                    DFS(steps + 1, path.ToString(), direction, data, finishedSteps);
+            if (direction.Item1 >= 0 && direction.Item1 < data.Length && direction.Item2 >= 0
+            && direction.Item2 < data[0].Length && data[direction.Item1][direction.Item2] != '#'
+            && !seen.Contains(direction))
+            {
+                seen.Add(direction);
+                //make the seen list unique for every path by copying it using ToList()
+                DFS(depth + 1, seen.ToList(), direction, target, data, paths);
+            }
         }
     }
 }
-
-Console.WriteLine(finishedSteps.Max());
